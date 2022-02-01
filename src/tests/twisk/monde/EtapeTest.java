4257d0b421cd55e24f5f81aa0ab01f7bb6d0b2a7
georgel27u <georhyunj>
2022-02-01 11:25:48 +0100

    Ajout de EtapeTest

---
+++ src/tests/twisk/monde/EtapeTest.java
@@ -0,0 +1,26 @@
+package twisk.monde;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EtapeTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void ajouterSuccesseur() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void estUneActivite() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void estUnGuicher() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void iterator() {
+    }
+}
