74942a967166074fe77f55f9e0f6bd415a85813f
georgel27u <georhyunj>
2022-02-01 11:58:59 +0100

    Ajout de tests

---
+++ src/tests/twisk/monde/GestionnaireEtapesTest.java
@@ -0,0 +1,25 @@
+package twisk.monde;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class GestionnaireEtapesTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void ajouter() {
+    }
+
+    @Test
+    void nbEtapes() {
+    }
+
+    @Test
+    void iterator() {
+    }
+}
---
+++ src/tests/twisk/monde/MondeTest.java
@@ -0,0 +1,37 @@
+package twisk.monde;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MondeTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void aCommeEntree() {
+    }
+
+    @Test
+    void aCommeSortie() {
+    }
+
+    @Test
+    void ajouter() {
+    }
+
+    @Test
+    void nbEtapes() {
+    }
+
+    @Test
+    void nbGuichets() {
+    }
+
+    @Test
+    void iterator() {
+    }
+}
