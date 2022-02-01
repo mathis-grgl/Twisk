6245307d78e9562223bf3752040dae5916df7716
georgel27u <georhyunj>
2022-02-01 11:05:23 +0100

    Ajout de commentaires dans Main.java

--- src/twisk/Main.java
+++ src/twisk/Main.java
@@ -10,14 +10,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        primaryStage.setTitle("Hello World"); //Met un titre à la fenêtre
+        primaryStage.setScene(new Scene(root, 300, 275)); //Définit la scène de la fenêtre avec root et les dimensions
+        primaryStage.show(); // Affiche et actualise la fenêtre
     }
 
 
     public static void main(String[] args) {
-        launch(args);
+        launch(args);//Lance l'interface
     }
 }
