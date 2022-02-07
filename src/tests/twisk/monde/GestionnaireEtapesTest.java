89fa0587aaf96414a0a6d06b8667e6e4ce58a136
Mathis <georgel27@etu.univ-lorraine.fr>
2022-02-07 20:06:13 +0100

    Tests

--- src/tests/twisk/monde/GestionnaireEtapesTest.java
+++ src/tests/twisk/monde/GestionnaireEtapesTest.java
@@ -6,20 +6,26 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GestionnaireEtapesTest {
+    private GestionnaireEtapes gE;
+    private Etape e1,e2,e3;
 
     @BeforeEach
     void setUp() {
+        gE = new GestionnaireEtapes();
+        Etape e1 = new Activite("e1");
+        Etape e2 = new Activite("e2");
+        Etape e3 = new Activite("e3");
     }
 
     @Test
     void ajouter() {
-    }
-
-    @Test
-    void nbEtapes() {
-    }
-
-    @Test
-    void iterator() {
+        gE.ajouter(e1);
+        assertEquals(gE.nbEtapes(),1,"bug dans ajouter()");
+        gE.ajouter(e2);
+        assertEquals(gE.nbEtapes(),2,"bug dans ajouter()");
+        gE.ajouter(e3);
+        assertEquals(gE.nbEtapes(),3,"bug dans ajouter()");
+        gE.ajouter(e1,e2,e3);
+        assertEquals(gE.nbEtapes(),6,"bug dans ajouter()");
     }
 }
--- src/tests/twisk/monde/GestionnaireSuccesseursTest.java
+++ src/tests/twisk/monde/GestionnaireSuccesseursTest.java
@@ -6,20 +6,28 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GestionnaireSuccesseursTest {
+    private GestionnaireSuccesseurs gE;
+    private Etape e1,e2,e3;
 
     @BeforeEach
     void setUp() {
+        gE = new GestionnaireSuccesseurs();
+        Etape e1 = new Activite("e1");
+        Etape e2 = new Activite("e2");
+        Etape e3 = new Activite("e3");
     }
 
     @Test
     void ajouter() {
+        gE.ajouter(e1);
+        assertEquals(gE.nbEtapes(),1,"bug dans ajouter()");
+        gE.ajouter(e2);
+        assertEquals(gE.nbEtapes(),2,"bug dans ajouter()");
+        gE.ajouter(e3);
+        assertEquals(gE.nbEtapes(),3,"bug dans ajouter()");
+        gE.ajouter(e1,e2,e3);
+        assertEquals(gE.nbEtapes(),6,"bug dans ajouter()");
     }
 
-    @Test
-    void nbEtapes() {
-    }
 
-    @Test
-    void iterator() {
-    }
 }
